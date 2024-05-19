6e6f9c09b7685046efcf1d87203e90c94e9c136c
Attah Christian <tuchris02@gmail.com>
2024-05-19 16:10:30 +0100

    styling

--- employment/settings.py
+++ employment/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-0^h_+nqhyh9=r81e)b#ns2@cn%$n_s%g#lapkz4v9mhz8+$4ua
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
